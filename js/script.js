e43dc6ed42242b51094def8c1cee4b39cfb5172d
Masa Wu <masastirn23@gmail.com>
2018-01-07 13:06:10 +0100

    typewriting effect js added

--- js/script.js
+++ js/script.js
@@ -262,11 +262,118 @@ function typeWriter() {
 var video = document.getElementById("video");
 video.addEventListener("transitionend",typeWriter2);
 var txt2 = '“Socrates said, “Know thyself.” I say, “Know thy users.” Joshua Brewer';
+var i2 = 0;
 
 function typeWriter2() {
-  if (i < txt2.length) {
-    document.getElementById("demo2").innerHTML += txt2.charAt(i);
-    i++;
+  if (i2 < txt2.length) {
+    document.getElementById("demo2").innerHTML += txt2.charAt(i2);
+    i2++;
     setTimeout(typeWriter2, speed);
   }
 }
+
+var viewfinders = document.getElementById("viewfinders");
+viewfinders.addEventListener("transitionend",typeWriter3);
+var txt3 = '"There are three responses to a piece of design. Yes, no and Wow! Wow is the one to aim for."  Milton Glaser';
+var i3 = 0;
+
+function typeWriter3() {
+  if (i3 < txt3.length) {
+    document.getElementById("demo3").innerHTML += txt3.charAt(i3);
+    i3++;
+    setTimeout(typeWriter3, speed);
+  }
+}
+
+var frank = document.getElementById("frank");
+frank.addEventListener("transitionend",typeWriter4);
+var txt4 = '“Simplicity is not the goal. It is the by-product of a good idea and modest expectations.” Paul Rand';
+var i4 = 0;
+
+function typeWriter4() {
+  if (i4 < txt4.length) {
+    document.getElementById("demo4").innerHTML += txt4.charAt(i4);
+    i4++;
+    setTimeout(typeWriter4, speed);
+  }
+}
+
+var drum = document.getElementById("drum");
+drum.addEventListener("transitionend",typeWriter5);
+var txt5 = '“What works good is better that what looks good, because what works good lasts.”  Ray Eames';
+var i5 = 0;
+
+function typeWriter5() {
+  if (i5 < txt5.length) {
+    document.getElementById("demo5").innerHTML += txt5.charAt(i5);
+    i5++;
+    setTimeout(typeWriter5, speed);
+  }
+}
+
+var menu2 = document.getElementById("menu");
+menu2.addEventListener("transitionend",typeWriter6);
+var txt6 = '“Technology may change rapidly, but people change slowly. The principals [of design] come from understanding of people. ”  D. Norman';
+var i6 = 0;
+
+function typeWriter6() {
+  if (i6 < txt6.length) {
+    document.getElementById("demo6").innerHTML += txt6.charAt(i6);
+    i6++;
+    setTimeout(typeWriter6, speed);
+  }
+}
+
+var misschili = document.getElementById("misschili");
+misschili.addEventListener("transitionend",typeWriter7);
+var txt7 = '“User-centered design means working with your users all throughout the project.”  D. Norman';
+var i7 = 0;
+
+function typeWriter7() {
+  if (i7 < txt7.length) {
+    document.getElementById("demo7").innerHTML += txt7.charAt(i7);
+    i7++;
+    setTimeout(typeWriter7, speed);
+  }
+}
+
+var redesign = document.getElementById("redesign");
+redesign.addEventListener("transitionend",typeWriter8);
+var txt8 = '“I strive for two things in design: simplicity and clarity. Great design is born of those two things.” L. Leader';
+var i8 = 0;
+
+function typeWriter8() {
+  if (i8 < txt8.length) {
+    document.getElementById("demo8").innerHTML += txt8.charAt(i8);
+    i8++;
+    setTimeout(typeWriter8, speed);
+  }
+}
+
+var swiss = document.getElementById("swiss");
+swiss.addEventListener("transitionend",typeWriter9);
+var txt9 = '“Good design is the act of communication between designer and the user”  D. Norman';
+var i9 = 0;
+
+function typeWriter9() {
+  if (i9 < txt9.length) {
+    document.getElementById("demo9").innerHTML += txt9.charAt(i9);
+    i9++;
+    setTimeout(typeWriter9, speed);
+  }
+}
+
+var home = document.getElementById("home");
+home.addEventListener("transitionend",typeWriter10);
+var txt10 = '“Design is a bridge between technology and people.”  D. Norman';
+var i10 = 0;
+
+function typeWriter10() {
+  if (i10 < txt10.length) {
+    document.getElementById("demo10").innerHTML += txt10.charAt(i10);
+    i10++;
+    setTimeout(typeWriter10, speed);
+  }
+}
+
+
